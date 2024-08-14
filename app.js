ed41aa6360ce7b659b29a47e6ee93208cbe6ccff
Muhammadxon Toshpolatov <muhammadxontoshpolatov@Muhammadxons-MacBook-Pro.local>
2024-08-14 09:12:08 +0500

    demo

---
+++ app.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector('#ham')
+let ul = document.querySelector('.ul')
+btn.addEventListener('click',() =>{
+    document.querySelector('.navs').classList.toggle('display')
+})
+ul.addEventListener('click',()=>{
+    document.querySelector('.navs').classList.toggle('display')
+
+})
